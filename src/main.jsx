ab341254e07224f4e1144507dda31389d0e7b25c
hitisha-goyani <goyanihitisha@gmail.com>
2025-07-30 23:07:01 +0530

    edit-movoe

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './rtk_querys/Store.js'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
